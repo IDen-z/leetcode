c916e1120fbb4dbbf601cf896a3876255b12864d
Secrets <zmzxdal@foxmail.com>
2022-06-24 08:41:49 +0800

    FEAT:提交git脚本

--- git-push.sh
+++ git-push.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-path=`pwd`
-git fetch origin
-echo 当前目录${path}项目已更新
-br=`git branch | grep "*"`
-cur=${br/* /}
-echo 当前分支为 ${cur}
-git add --all
-git commit -m $1
-git merge origin/${cur} ${cur}
-git push origin ${cur}
-echo 推送远程仓库完成！
+if [ ! -n "$1" ] ;
+then
+  echo 请输入提交信息
+else
+  path=`pwd`
+  git fetch origin
+  echo 当前目录${path}项目已更新
+  br=`git branch | grep "*"`
+  cur=${br/* /}
+  echo 当前分支为 ${cur}
+  git add --all
+  git commit -m $1
+  git merge origin/${cur} ${cur}
+  git push origin ${cur}
+  echo 推送远程仓库完成！
+fi
+
 
