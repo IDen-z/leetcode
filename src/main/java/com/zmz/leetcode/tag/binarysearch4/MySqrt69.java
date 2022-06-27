d4cf41f7bd9ddb0d49314a41a21097c06121cec7
Secrets <zmzxdal@foxmail.com>
2022-06-27 20:06:28 +0800

    FEAT:leetcode每日提交

--- src/main/java/com/zmz/leetcode/tag/binarysearch4/MySqrt69.java
+++ src/main/java/com/zmz/leetcode/tag/binarysearch4/MySqrt69.java
@@ -13,10 +13,18 @@ public class MySqrt69 {
      * 注意：不允许使用任何内置指数函数和算符，例如 pow(x, 0.5) 或者 x ** 0.5 。
      */
     public int mySqrt(int x) {
-
-
-
-
+        // 官方题解 其实可以理解成 x*x <= k 找到第一个x就是正确答案
+        int l = 0, r = x, ans = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if ((long) mid * mid <= x) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
     }
 
 
