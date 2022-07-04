2d2898377c4832a5d9969352e82a18dceed3971d
Secrets <zmzxdal@foxmail.com>
2022-07-04 15:23:42 +0800

    FEAT:leetcode每日提交

--- src/main/java/com/zmz/leetcode/hot/FindMedianSortedArrays4.java
+++ src/main/java/com/zmz/leetcode/hot/FindMedianSortedArrays4.java
@@ -1,5 +1,7 @@
 package com.zmz.leetcode.hot;
 
+import com.sun.org.apache.xpath.internal.FoundIndex;
+
 /**
  * 寻找两个正序数组的中位数
  */
@@ -15,9 +17,58 @@ public class FindMedianSortedArrays4 {
 
         // 寻找两个正序数组的中位数
         // sortedArray + 寻找  想到了二分查找
+        // 但是这是两个数组 暴力自然是数组合并后利用二分
+        int m = nums1.length;
+        int n = nums2.length;
+        int temp[] = new int[m + n];
+        int index1 = 0;
+        int index2 = 0;
+        int k = 0;
+        while (index1 < m && index2 < n) {
+            if (nums1[index1] < nums2[index2]) {
+                temp[k++] = nums1[index1++];
+            } else {
+                temp[k++] = nums2[index2++];
+            }
+        }
+        while (index1 < m) {
+            temp[k++] = nums1[index1++];
+        }
+        while (index2 < n) {
+            temp[k++] = nums2[index2++];
+        }
+        // 接下来直接找中间数
+        int x = (m + n) / 2;
+        if (x == 0) {
+            return temp[0];
+        }
+        if ((m + n) % 2 == 0) {
+            return (double) (temp[x - 1] + temp[x]) / 2;
+        } else {
+            return temp[x];
+        }
+    }
 
+    /**
+     * 官方题解
+     */
+    public double findMedianSortedArraysOff(int[] nums1, int[] nums2) {
+        // 官网题解
+        // 如果对时间复杂度的要求有 log，通常都需要用到二分查找，这道题也可以通过二分查找实现。
+        // 根据中位数的定义，当 m+n 是奇数时，中位数是两个有序数组中的第 (m+n)/2 个元素，
+        // 当 m+n 是偶数时，中位数是两个有序数组中的第(m+n)/2 个元素和第 (m+n)/2+1 个元素的平均值。因此，这道题可以转化成寻找两个有序数组中的第 k 小的数，其中 kk 为 (m+n)/2或 (m+n)/2+1。
 
         return 0;
+    }
+
+    public static void main(String[] args) {
+        FindMedianSortedArrays4 findMedianSortedArrays4 = new FindMedianSortedArrays4();
+        int[] n1 = new int[]{1, 3, 5};
+        int[] n2 = new int[]{2, 7};
+        double medianSortedArrays = findMedianSortedArrays4.findMedianSortedArrays(n1, n2);
+        System.err.println(medianSortedArrays);
+
+
     }
 
 
---
+++ src/main/java/com/zmz/leetcode/hot/LongestPalindrome5.java
@@ -0,0 +1,17 @@
+package com.zmz.leetcode.hot;
+
+/**
+ * 最长回文子串
+ */
+public class LongestPalindrome5 {
+
+    /**
+     * 给你一个字符串 s，找到 s 中最长的回文子串。
+     */
+    public String longestPalindrome(String s) {
+        // 动态规划的经典题目 一定收藏再做
+        return null;
+    }
+
+
+}
