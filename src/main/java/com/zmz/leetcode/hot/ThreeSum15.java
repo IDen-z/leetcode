3c3b81bdbf01954338ea25de3c5547d475e7a18f
Secrets <zmzxdal@foxmail.com>
2022-07-09 13:05:30 +0800

    leetcode每日提交

--- src/main/java/com/zmz/leetcode/hot/ThreeSum15.java
+++ src/main/java/com/zmz/leetcode/hot/ThreeSum15.java
@@ -26,16 +26,34 @@ public class ThreeSum15 {
         int l = nums.length;
         int index = l - 1;
         for (int i = 0; i < l; i++) {
+            boolean flag = false;
             for (int j = i + 1; j < l; j++) {
+//                if (flag) {
+//                    break;
+//                }
+                if (nums[i] > 0) {
+                    break;
+                }
+                //去重，当起始的值等于前一个元素，那么得到的结果将会和前一次相同
+//                if (i > 0 && nums[i] == nums[i - 1]) continue;
+                if (nums[j] == nums[j - 1]) continue;
                 index = l - 1;
                 while (j != index) {
+                    if (res.size() != 0 && nums[j] == nums[j - 1]) {
+                        break;
+                    }
+                    if (res.size() != 0 && index < l - 1 && nums[index] == nums[index + 1]) {
+                        index--;
+                        continue;
+                    }
                     if (nums[i] + nums[j] + nums[index] == 0) {
                         ArrayList<Integer> list = new ArrayList<>();
                         list.add(nums[i]);
                         list.add(nums[j]);
                         list.add(nums[index]);
                         res.add(list);
-                        break;
+                        index--;
+//                        flag = true;
                     } else {
                         index--;
                     }
@@ -45,9 +63,50 @@ public class ThreeSum15 {
         return res;
     }
 
+    /**
+     * 查看了别人的题解有的思路
+     */
+    public List<List<Integer>> threeSumOff(int[] nums) {
+        // 可以先思考  如何找到不重复的二元数组的
+        // 再模拟寻找三元数组 其实就相当于 双指针找二元数组的外面加了一层循环而已
+        // 关键在于去重
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        int l = nums.length;
+        for (int i = 0; i < l; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            // 内层双指针循环
+            int left = i + 1;
+            int right = l - 1;
+            while (left < right) {
+                if (nums[left] + nums[right] == -nums[i]) {
+                    ArrayList<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[left]);
+                    list.add(nums[right]);
+                    res.add(list);
+                    left++;
+                    right--;
+                    while (left > 0 && left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                    while (right < l - 1 && left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                } else if (nums[left] + nums[right] < -nums[i]) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return res;
+    }
+
+
     public static void main(String[] args) {
         ThreeSum15 threeSum15 = new ThreeSum15();
-        threeSum15.threeSum(new int[]{-1, 0, 1, 2, -1, -4});
+        threeSum15.threeSumOff(new int[]{-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4});
     }
 
 }
