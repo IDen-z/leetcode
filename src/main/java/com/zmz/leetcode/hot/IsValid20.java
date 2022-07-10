6bb83a655674491cc6274b7766c533b05e1dbddb
Secrets <zmzxdal@foxmail.com>
2022-07-11 00:02:50 +0800

    每日提交

---
+++ src/main/java/com/zmz/leetcode/hot/IsValid20.java
@@ -0,0 +1,24 @@
+package com.zmz.leetcode.hot;
+
+/**
+ * 有效的括号
+ */
+public class IsValid20 {
+
+
+    /**
+     * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+     * <p>
+     * 有效字符串需满足：
+     * <p>
+     * 左括号必须用相同类型的右括号闭合。
+     * 左括号必须以正确的顺序闭合。
+     */
+    public boolean isValid(String s) {
+        // 该题已经做过
+
+        return false;
+    }
+
+
+}
