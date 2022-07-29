8f81c4bc49ae700d1409757b95309271a6e76aa2
Secrets <zmzxdal@foxmail.com>
2022-07-29 17:32:17 +0800

    leetcode每日提交

--- src/main/java/com/zmz/leetcode/hot/MaxProduct152.java
+++ src/main/java/com/zmz/leetcode/hot/MaxProduct152.java
@@ -1,5 +1,7 @@
 package com.zmz.leetcode.hot;
 
+import org.jcp.xml.dsig.internal.dom.DOMUtils;
+
 /**
  * 乘积最大子数组
  */
@@ -30,9 +32,29 @@ public class MaxProduct152 {
 
     /**
      * 也可以采用动态规划
-     *
+     * <p>
      * 只需要同时定义两个dp数组即可
      */
-
+    public static void main(String[] args) {
+        // 基数年薪*1%
+        double base = 2500;
+        // 年薪预计增幅 预计每年涨薪2W
+        double improve = 200;
+        // 每年的投资收益率 5%
+        double ROI = 0.05;
+        // 离退休的年限
+        int endYear = 35;
+        double amountIncome = 0.0;
+        int amountBase = 0;
+        for (int i = 0; i < endYear; i++) {
+            int count = i * 200;
+            Double income = (count + base) * Math.pow(1 + ROI, endYear - i);
+            System.err.println("每年的投资收益\n" + income);
+            amountIncome += income;
+            amountBase += base;
+        }
+        System.err.println("总成本" + amountBase);
+        System.err.println("总收益" + amountIncome);
+    }
 
 }
