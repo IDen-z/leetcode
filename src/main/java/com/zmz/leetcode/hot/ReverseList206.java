da3b8c639bb9826b75425c79216a15e01d3e65c0
Secrets <zmzxdal@foxmail.com>
2022-08-01 09:47:58 +0800

    leetcode每日提交

---
+++ src/main/java/com/zmz/leetcode/hot/ReverseList206.java
@@ -0,0 +1,34 @@
+package com.zmz.leetcode.hot;
+
+/**
+ * 反转链表
+ */
+public class ReverseList206 {
+
+
+    /**
+     * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+     */
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(3);
+        a.next = b;
+        b.next = c;
+        ReverseList206 reverseList206 = new ReverseList206();
+        ListNode node = reverseList206.reverseList(a);
+    }
+
+}
