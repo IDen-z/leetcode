acaa87e755c1c8b8cf7642ec5b956486ee4be352
Secrets <zmzxdal@foxmail.com>
2022-08-08 17:28:33 +0800

    leetcode每日提交

--- src/main/java/com/zmz/leetcode/hot/SearchMatrix240.java
+++ src/main/java/com/zmz/leetcode/hot/SearchMatrix240.java
@@ -12,11 +12,55 @@ public class SearchMatrix240 {
      * 每列的元素从上到下升序排列。
      */
     public boolean searchMatrix(int[][] matrix, int target) {
-
-
-
-
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][0] > target) {
+                break;
+            }
+            if(matrix[i][matrix[i].length - 1] < target){
+                continue;
+            }
+            int col = binarySearch(matrix[i], target);
+            if (col != -1) {
+                return true;
+            }
+        }
         return false;
+    }
+
+    //二分查找
+    private int binarySearch(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start < end) {
+            int mid = (start + end) >>> 1;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid ;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{
+                {1, 2, 3, 4, 5},
+                {6, 7, 8, 9, 10},
+                {11, 12, 13, 14, 15},
+                {16, 17, 18, 19, 20},
+                {21, 22, 23, 24, 25}
+        };
+//        int[][] matrix = new int[][]{
+//                {1,1}
+//        };
+        SearchMatrix240 searchMatrix240 = new SearchMatrix240();
+        searchMatrix240.searchMatrix(matrix, 19);
+
     }
 
 
