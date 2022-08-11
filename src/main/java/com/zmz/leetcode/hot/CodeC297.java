375a174b0fc18d906e5bb5c0ce48b800938b10f8
Secrets <zmzxdal@foxmail.com>
2022-08-11 15:54:14 +0800

    leetcode每日提交

--- src/main/java/com/zmz/leetcode/hot/CodeC297.java
+++ src/main/java/com/zmz/leetcode/hot/CodeC297.java
@@ -1,5 +1,9 @@
 package com.zmz.leetcode.hot;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 二叉树的序列化与反序列化
  */
@@ -10,18 +14,60 @@ public class CodeC297 {
      * <p>
      * 请设计一个算法来实现二叉树的序列化与反序列化。这里不限定你的序列 / 反序列化算法执行逻辑，你只需要保证一个二叉树可以被序列化为一个字符串并且将这个字符串反序列化为原始的树结构。
      */
-
+    String res = "";
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
+        // 这道题解题的关键在于前序遍历一定不能停在当前节点 要多遍历一层 把null的也记录下来
+        dfsSearch(root);
+        return res;
+    }
 
-        return null;
+    private void dfsSearch(TreeNode node) {
+        if (node == null) {
+            res += "null,";
+            return;
+        }
+        res += node.val + ",";
+        dfsSearch(node.left);
+        dfsSearch(node.right);
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
+        ArrayDeque<String> deque = new ArrayDeque<>();
+        String[] split = data.split(",");
+        for (String s : split) {
+            deque.addLast(s);
+        }
+        // 在反序列化字符串时
+        TreeNode root = dfsHelper(deque);
+        return root;
+    }
 
-        return null;
+    private TreeNode dfsHelper(ArrayDeque<String> deque) {
+        if (deque.isEmpty()) {
+            return null;
+        }
+        String s = deque.removeFirst();
+        if (s.equals("null")) {
+            return null;
+        }
+        TreeNode node = new TreeNode(Integer.parseInt(s));
+        node.left = dfsHelper(deque);
+        node.right = dfsHelper(deque);
+        return node;
+    }
+
+
+    public static void main(String[] args) {
+        Integer[] nums = new Integer[]{1, 2, 3, null, null, 4, 5};
+        TreeNode tree = TreeUtil.createTree(nums);
+        CodeC297 codeC297 = new CodeC297();
+        String serialize = codeC297.serialize(tree);
+        System.err.println(serialize);
+        TreeNode deserialize = codeC297.deserialize("1,2,null,null,3,4,null,null,5,null,null");
+        System.err.println(deserialize);
     }
 
 
---
+++ src/main/java/com/zmz/leetcode/hot/RemoveInvalidParentheses301.java
@@ -0,0 +1,23 @@
+package com.zmz.leetcode.hot;
+
+import java.util.List;
+
+/**
+ * 删除无效的括号
+ */
+public class RemoveInvalidParentheses301 {
+
+    /**
+     * 给你一个由若干括号和字母组成的字符串 s ，删除最小数量的无效括号，使得输入的字符串有效。
+     *
+     * 返回所有可能的结果。答案可以按 任意顺序 返回。
+     */
+    public List<String> removeInvalidParentheses(String s) {
+
+
+        return null;
+    }
+
+
+
+}
