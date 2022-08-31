e4d6f7bb70177b064362974eb5b1d60473c55143
Secrets <zmzxdal@foxmail.com>
2022-08-31 10:53:02 +0800

    leetcode每日提交

---
+++ src/main/java/com/zmz/leetcode/hot/SubarraySum560.java
@@ -0,0 +1,27 @@
+package com.zmz.leetcode.hot;
+
+/**
+ * 和为k的子数组
+ */
+public class SubarraySum560 {
+
+    /**
+     * 给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的连续子数组的个数 。
+     */
+    public int subarraySum(int[] nums, int k) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum == k) {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+}
+
+
+
