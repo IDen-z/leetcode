c15fc1e48a837c6620e5b1af065a85fcd552d509
Secrets <zmzxdal@foxmail.com>
2022-10-31 19:41:37 +0800

    leetcode每日提交

---
+++ src/main/java/com/zmz/leetcode/daily/SumSubarrayMins907.java
@@ -0,0 +1,31 @@
+package com.zmz.leetcode.daily;
+
+/**
+ * 子数组的最小值之和
+ */
+public class SumSubarrayMins907 {
+
+    /**
+     * 给定一个整数数组 arr，找到 min(b) 的总和，其中 b 的范围为 arr 的每个（连续）子数组。
+     *
+     * 由于答案可能很大，因此 返回答案模 10^9 + 7 。
+     *
+     */
+    public int sumSubarrayMins(int[] arr) {
+        // 先求子数组
+
+
+        // 再求子数组中的最小值
+
+        // 再将最小值求和
+
+
+
+
+        return 0;
+    }
+
+
+
+
+}
