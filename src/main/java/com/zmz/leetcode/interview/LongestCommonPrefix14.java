b6cd5a78cfa1e1b0d807988e6e27b2d7842ea451
Secrets <zmzxdal@foxmail.com>
2024-08-07 09:35:54 +0800

    leetcode每日提交

--- src/main/java/com/zmz/leetcode/interview/LongestCommonPrefix14.java
+++ src/main/java/com/zmz/leetcode/interview/LongestCommonPrefix14.java
@@ -1,6 +1,11 @@
 package com.zmz.leetcode.interview;
 
 public class LongestCommonPrefix14 {
+    public static void main(String[] args) {
+        String[] srts = new String[]{"do", "d"};
+        String s = longestCommonPrefix(srts);
+        System.err.println(s);
+    }
 
     // 编写一个函数来查找字符串数组中的最长公共前缀。
     //
@@ -18,19 +23,24 @@ public class LongestCommonPrefix14 {
     //输出：""
     //解释：输入不存在公共前缀。
 
-    public String longestCommonPrefix(String[] strs) {
+    public static String longestCommonPrefix(String[] strs) {
         // 遍历
-        if (strs.length <= 0) {
+        if (strs.length == 0) {
             return "";
         }
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < strs[0].length(); i++) {
             Character c = strs[0].charAt(i);
+            int j = 1;
+            while (j < strs.length) {
+                if (strs[j].length() <= i || c != strs[j].charAt(i)) {
+                    return sb.toString();
+                }
+                j++;
+            }
             sb.append(c);
-
         }
-
-        return null;
+        return sb.toString();
     }
 
 
