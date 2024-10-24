9df73b4bc9a6353201f1481673e5f4d81f2552c0
Secrets <zmzxdal@foxmail.com>
2024-10-24 14:25:23 +0800

    leetcode每日提交

--- src/main/java/com/zmz/leetcode/interview/Permute46.java
+++ src/main/java/com/zmz/leetcode/interview/Permute46.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Permute46 {
+
+
     /**
      * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
      * <p>
@@ -22,30 +24,35 @@ public class Permute46 {
      * 输入：nums = [1]
      * 输出：[[1]]
      */
-
+    List<List<Integer>> res = new ArrayList<>();
 
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        // 全排列 直接递归 dfs
-        dfsHelper(nums, res, list);
+        // 生成可能的全排列
+        // dfs递归
+
+        dfsHelper(nums, new ArrayList<>());
+
         return res;
     }
 
-    private void dfsHelper(int[] nums, List<List<Integer>> res, List<Integer> list) {
+    private void dfsHelper(int[] nums, ArrayList<Integer> list) {
         // 递归终止条件
         if (list.size() == nums.length) {
             res.add(new ArrayList<>(list));
-            return;
         }
+        // 递归
+        // 先循环还是先加入list
+
         for (int i = 0; i < nums.length; i++) {
-            if (list.contains(nums[i])){
+            if (list.contains(nums[i])) {
                 continue;
             }
+            // 不包含 就把 他加入list
             list.add(nums[i]);
-            dfsHelper(nums, res, list);
+            dfsHelper(nums, list);
             list.remove(list.size() - 1);
         }
+
     }
 
 
