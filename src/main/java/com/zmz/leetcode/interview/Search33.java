65dc5410379e1c81772dd045c6a34236acd3fe38
Secrets <zmzxdal@foxmail.com>
2024-12-12 10:03:05 +0800

    leetcode每日提交

---
+++ src/main/java/com/zmz/leetcode/interview/Search33.java
@@ -0,0 +1,97 @@
+package com.zmz.leetcode.interview;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 整数数组 nums 按升序排列，数组中的值 互不相同 。
+ * <p>
+ * 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为 [4,5,6,7,0,1,2] 。
+ * <p>
+ * 给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回 -1 。
+ * <p>
+ * 你必须设计一个时间复杂度为 O(log n) 的算法解决此问题。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [4,5,6,7,0,1,2], target = 0
+ * 输出：4
+ * 示例 2：
+ * <p>
+ * 输入：nums = [4,5,6,7,0,1,2], target = 3
+ * 输出：-1
+ * 示例 3：
+ * <p>
+ * 输入：nums = [1], target = 0
+ * 输出：-1
+ */
+
+public class Search33 {
+
+    public static void main(String[] args) {
+        Search33 bean = new Search33();
+        bean.search(new int[]{3, 1}, 1);
+    }
+
+    public int search(int[] nums, int target) {
+        // 这启示我们可以在常规二分查找的时候查看当前 mid 为分割位置分割出来的两个部分 [l, mid] 和 [mid + 1, r] 哪个部分是有序的，
+        // 并根据有序的那个部分确定我们该如何改变二分查找的上下界，因为我们能够根据有序的那部分判断出 target 在不在这个部分：
+        //
+        // 如果 [l, mid - 1] 是有序数组，且 target 的大小满足 [nums[l],nums[mid])，则我们应该将搜索范围缩小至 [l, mid - 1]，否则在 [mid + 1, r] 中寻找。
+        // 如果 [mid, r] 是有序数组，且 target 的大小满足 (nums[mid+1],nums[r]]，则我们应该将搜索范围缩小至 [mid + 1, r]，否则在 [l, mid - 1] 中寻找。
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            // TODO 这里必须是 大于 等于
+            // 原因是如果是等于  也会进左序列进行查找
+            // 如果是else 进入右序列，那么nums[mid]=nums[left]的情况就会找不到
+            // 因为等于的情况也要进入左序列查找
+            if (nums[left] <= nums[mid]) {
+                // 说明左边是上升区间  普通二分
+                if (nums[mid] > target && target >= nums[left]) {
+                    // 在左侧
+                    right = mid - 1;
+                } else {
+                    // 在右侧
+                    left = mid + 1;
+                }
+            } else {
+                // 右边是上升区间
+                if (nums[mid] <= target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+
+            }
+
+        }
+
+        return -1;
+    }
+
+
+    // 普通二分
+    public int searchCommon(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+
+    }
+
+}
